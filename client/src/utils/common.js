e8deecd0463e358af47ab06ef3619e235d2ed054
Naveen Kumar <naveensakthivel3251@gmail.com>
2023-10-22 11:31:37 +0530

    FrontEnd

---
+++ client/src/utils/common.js
@@ -0,0 +1,20 @@
+//slides view based on device screen
+//specially for swiper and these are inbuilt properties of swiper
+export const sliderSettings={
+    slidesPerView:1,
+    spaceBetween:50,
+    breakpoints:{
+        480:{
+            slidesPerView:1
+        },
+        600:{
+            slidesPerView:2
+        },
+        750:{
+            slidesPerView:3
+        },
+        1100:{
+            slidesPerView:4
+        }
+    }
+}
